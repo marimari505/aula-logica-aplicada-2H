000b6497d3039a3442f83bac6d10940c9f9ca85b
marimari505 <mariacsoares.2801@gmail.com>
2023-05-04 09:54:24 -0300

    Aula introdução OO

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int contador = 0;
+        // Aula introdutória de Orientação á Objetos
 
-        while (contador < 10) {
-            System.out.println("Contador -= " + contador);
-            // Incremento
-            contador = contador + 1;
-        }
 
-        // laço para faça
-        for(int x = 0; x < 10; x++){
-            System.out.println("Contador = " +x);
-        }
     }
-}
+    }
